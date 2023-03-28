ab5d714208888f0649c54da67e0dab9a8181f788
Neider Pandales <napandales@indracompany.com>
2023-03-28 06:45:48 -0500

    Example matchers

---
+++ example_macthers/constantes.js
@@ -0,0 +1,57 @@
+const electrodomesticos = [
+  {
+    refrigeracion: [
+      'Neveras',
+      'Nevecones',
+      'Congeladores',
+      'Mini neveras',
+      'Mini Bar'
+    ]
+  },
+  {
+    lavadores_secadoras: [
+      'Lavadoras',
+      'Secadoras',
+      'Lavadores secadoras'
+    ]
+  },
+  {
+    cocina: [
+      'Estufas',
+      'Microondas',
+      'Licuadoras'
+    ]
+  }
+]
+
+const electrodomesticos_copia = [
+  {
+    refrigeracion: [
+      'Neveras',
+      'Nevecones',
+      'Congeladores',
+      'Mini neveras',
+      'Mini Bar'
+    ]
+  },
+  {
+    lavadores_secadoras: [
+      'Lavadoras',
+      'Secadoras',
+      'Lavadores secadoras'
+    ]
+  },
+  {
+    cocina: [
+      'Estufas',
+      'Microondas',
+      'Licuadoras'
+    ]
+  }
+]
+
+
+module.exports = {
+  electrodomesticos,
+  electrodomesticos_copia
+};
